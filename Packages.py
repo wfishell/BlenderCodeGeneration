35289a2f1814466bb5eaae45f63a2028df1ce29f
Will Fishell <will@Wills-MBP-9.lan>
2024-11-09 21:53:01 -0500

    please work

---
+++ Packages.py
@@ -0,0 +1,36 @@
+import openai
+import autopep8
+import re
+import os
+import pickle
+import google.auth
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google.oauth2 import service_account
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+import json
+import textwrap
+import time
+from datetime import datetime
+import sys
+import base64
+import cv2
+import argparse
+import io
+import tempfile
+from googleapiclient.http import MediaIoBaseUpload
+from googleapiclient.http import MediaIoBaseDownload
+from googleapiclient.discovery import build
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import traceback
+import pandas as pd
+import ast 
+from io import StringIO
+import seaborn as sns
+import matplotlib.pyplot as plt
+# OPEN AI API KEY
+openai.api_key = ()
+
